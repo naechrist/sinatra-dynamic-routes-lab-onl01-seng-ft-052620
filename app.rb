7a68c2d76d172dc72dc41d5af6785409c09e6633
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-06 23:51:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do 
-    @user_name = params[:name]
-    
+    @reverse_name = params[:name].reverse 
+    @reverse_name
+  end 
+  
+  get '/square/:number' do 
+    @num = params[:number].to_i ** 2 
+    @num.to_s 
   end 
 
 end
